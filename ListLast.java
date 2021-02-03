34e68dd090e5f629fdcb4688d1ff52044ce0dac4
Rachel Lee <600021256@fjuhsd.org>
2021-02-03 22:28:28 +0000

    Unit 7 Lab 2, method that checks if last value in list occurs more than once.

--- ListLast.java
+++ ListLast.java
@@ -1,15 +1,32 @@
 //(c) A+ Computer Science
 //www.apluscompsci.com
-//Name -
-//Date -
+//Name - Rachel Lee
+//Date - 02/03/2021
 
 import java.util.List;
 import java.util.ArrayList;
 
 public class ListLast
 {
-	public static boolean go( List<Integer> ray)
+	//Method that checks if the last value in the list occurs more than once, returns a boolean
+  //Parameter: Integer list
+  public static boolean go( List<Integer> ray)
 	{
-		return true;
+    //Initialize and declare boolean variable named lastVal, set to false
+    boolean lastVal = false;
+    //For loop: loop control variable Integer i, loop as long as i is less than size - 1, i increments by 1
+    for(Integer i = 0; i < ray.size() - 1; i++)
+    {
+      //if i equals the element at index size-1 of ray
+      if(ray.get(i).equals(ray.get(ray.size()-1)))
+      {
+        //set lastVal to true
+        lastVal = true;
+        //return lastVal
+        return lastVal;
+      }
+    }
+    //return lastVal
+    return lastVal;
 	}
 }
